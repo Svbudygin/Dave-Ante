fc0febd25e23a126015b1236441bd2aad7f6c2c5
sergeybudygin <svbudygin@yandex.ru>
2024-02-19 10:36:30 +0300

    Done

--- mail.py
+++ mail.py
@@ -9,7 +9,7 @@ from config import password_mail
 
 
 def send_email(to_mail, code):
-    sender = "pokerdsba@gmail.com"
+    sender = "only.for.creators77@gmail.com"
     template = None
     password =password_mail
 
@@ -20,7 +20,6 @@ def send_email(to_mail, code):
         server.login(sender, password)
         msg["From"] = sender
         msg["To"] = to_mail
-        # msg.attach(MIMEText(template, "html"))
         msg.attach(MIMEText(str(code)))
         msg["Subject"] = "Mail confirmation"
         server.sendmail(sender, to_mail, msg.as_string())
@@ -30,7 +29,7 @@ def send_email(to_mail, code):
 
 
 def send_email_with_photo(to_mail, code, file_url):
-    sender = "pokerdsba@gmail.com"
+    sender = "only.for.creators77@gmail.com"
     password = password_mail
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -41,16 +40,12 @@ def send_email_with_photo(to_mail, code, file_url):
         msg["From"] = sender
         msg["To"] = to_mail
         msg["Subject"] = "Mail confirmation"
-        msg.attach(MIMEText(str(code)))  # Текст письма
-
-        # Получаем информацию о файле
+        msg.attach(MIMEText(str(code)))
 
         response = requests.get(file_url)
 
-        # Конвертируем содержимое файла в base64 строку
         photo_base64 = base64.b64encode(response.content).decode('utf-8')
 
-        # Добавляем фото как вложение к письму
         image = MIMEImage(base64.b64decode(photo_base64), name='photo.jpg')
         msg.attach(image)
 
